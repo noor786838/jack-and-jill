d8ad25946b181cdc1a91d0d8ebc9d65421c8b594
noor786838 <63953012+noor786838@users.noreply.github.com>
2020-04-20 17:57:01 +0530

    to give marks
    to create marks list may be

---
+++ project2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter marks");
+    scanf("%d",&a);
+    if(a>=85)
+{
+     printf("grade A");
+}
+      else if(a>=70)
+      {
+      printf("grade B);
+}
+      else if(a>=55)
+      {
+      printf("grade C);
+}
+      else if(a>=40)
+      {
+       printf("grade D);
+}
+      else if(a<=40)
+      {
+       printf("grade E");
+ }
+  return 0;
+  }
